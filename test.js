05b1486b222818e89023dfbe57bb0e780979fcfa
Maciej Langowski <maciej.langowski234@gmail.com>
2020-02-25 19:18:36 +0100

    zaktualizowano test.js po filmie 12 - if statements

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-alert("Hello world!!!");
+var youLikeMeat = true;
+var myNum = 10;
+
+if (youLikeMeat) {
+    document.write("You like meat");
+    }
+else {  
+    document.write("You hate meat");
+}
