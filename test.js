d9792f8b51215ceb7bff1682f593459f9d3dcc55
Maciej Langowski <maciej.langowski234@gmail.com>
2020-02-25 19:38:15 +0100

    zaktualizowano test.js po filmie 13 - else if

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*
 var youLikeMeat = true;
 var myNum = 10;
 
@@ -7,3 +8,22 @@ if (youLikeMeat) {
 else {  
     document.write("You hate meat");
 }
+*/
+
+var myAge = 45;
+
+if (myAge > 25){
+    document.write("you are over 25");
+}
+
+else if (myAge > 30){
+    document.write("you are over 30");
+}
+
+else if (myAge > 10){
+    document.write("you are over 10");
+}
+
+else{
+    document.write("you are not older than 10");
+    }
