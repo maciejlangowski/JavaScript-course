04fcce400ece81e977c4a498f1f30552869e303c
Maciej Langowski <maciej.langowski234@gmail.com>
2020-02-27 19:49:12 +0100

    test.js po filmie 29,30. - constructor functions

--- test.js
+++ test.js
@@ -137,10 +137,50 @@ else{
     myString.indexOf("string"));
 }
 */
-
+/*
 var string1 = "abc";
 var string2 = "bcd";
 
 console.log(string1.toLowerCase === string2.toLowerCase);
 
 console.log(string1 > string2);
+
+*/
+/*
+var myCar3 = {
+
+    
+    maxSpeed: 50, 
+    driver: "Maciek", 
+    drive: function(speed, time)
+        {console.log(speed * time);},
+
+    logDriver: function(){
+    console.log("driver name is " + this.driver) ;
+    }}
+    myCar3.drive(50, 2); 
+myCar3.logDriver();
+*/
+/* CONSTRUCTOR FUNCTIONS
+var myArray = new Array(); 
+var myString = new String();
+var myCar = new Car();
+*/
+
+var Car = function(maxSpeed, driver){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time)
+            {console.log(speed * time);}
+
+    this.logDriver = function(){
+        console.log("driver name is " + this.driver) ;
+}
+}
+var myCar = new Car(70,"Maciek");
+var myCar2 = new Car(50,"Waciek");
+var myCar3 = new Car(60,"Mdddk");
+var myCar4 = new Car(70,"Mddfdfk");
+
+myCar.drive(30,5);
+myCar3.logDriver();
